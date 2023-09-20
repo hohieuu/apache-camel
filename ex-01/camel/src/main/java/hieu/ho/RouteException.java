5b037411af127e60047c6daff69a0567623ebfdb
Hieu Ho <55455388+hohieuu@users.noreply.github.com>
2023-09-20 15:40:58 +0700

    reformat code

--- ex-01/camel/src/main/java/hieu/ho/RouteException.java
+++ ex-01/camel/src/main/java/hieu/ho/RouteException.java
@@ -2,26 +2,26 @@ package hieu.ho;
 
 import org.apache.camel.builder.RouteBuilder;
 
-class CustomGrpcError extends Exception{}
+class CustomGrpcError extends Exception {}
+
 public class RouteException extends RouteBuilder {
 
   public static int count = 0;
 
   @Override
   public void configure() throws Exception {
-          onException(CustomGrpcError.class)
+    onException(CustomGrpcError.class)
       .maximumRedeliveries(100)
       .redeliveryDelay(0)
       .to("direct:exception");
 
-      onException(Exception.class)
-      .log("other exception");
+    onException(Exception.class).log("other exception");
 
     from("timer://hello?fixedRate=true&period=10000").to("direct:start");
 
     from("direct:start")
-      .routeId("testException").
-      doTry()
+      .routeId("testException")
+      .doTry()
       .process(exchange -> {
         count++;
         log.info("Retry: " + count);
@@ -29,15 +29,14 @@ public class RouteException extends RouteBuilder {
         if (count < 200) {
           throw new NullPointerException("null pointer exception");
         }
-      }).
-      to("direct:success").
-      doCatch(Exception.class)
+      })
+      .to("direct:success")
+      .doCatch(Exception.class)
       .throwException(new CustomGrpcError())
       .end()
       .throwException(new Exception());
 
     from("direct:exception").log("log failed");
     from("direct:success").log("log success");
-
   }
 }
